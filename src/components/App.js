550b8145519e1b806d7e55bd8a221b7484ebe5f8
Yi-Shin lin <yishinlin@outlook.com>
2019-03-19 15:25:33 +0800

    Finish Navigation with React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,25 @@
 import React from "react";
-import { BrowserRouter, Route ,Link } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from './Header';
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne {/*<a href="/pagetwo">PageTwo</a>*/}  
-      <Link to='/pagetwo'>PageTwo</Link>
-    </div>
-  );
-};
-//Bad to use anchor tag
-const PageTwo = () => {
-  return (
-    <div>
-      <Link to="/">PageOne</Link> PageTwo
-    </div>
-  );
-};
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
+        <Header />
         <div>
-          <Route path="/" exact component={PageOne} />
+          <Route path="/" exact component={StreamList} />
           {/*<Route path="/" exact component={PageOne} /> HTML宣告兩個就會產生兩個併排*/}
-          <Route path="/pagetwo" exact component={PageTwo} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
