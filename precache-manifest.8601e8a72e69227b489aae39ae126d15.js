419d080414dd1c532427b8c74e01c305fbc7f48a
Yi-Shin lin <yishinlin@outlook.com>
2019-03-19 18:02:08 +0800

    Updates

---
+++ precache-manifest.8601e8a72e69227b489aae39ae126d15.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fcf66e605dbddd07ab8",
+    "url": "/streams-client/static/js/runtime~main.b97f421e.js"
+  },
+  {
+    "revision": "80a6d8b65a74d4dc7cd8",
+    "url": "/streams-client/static/js/main.10a56143.chunk.js"
+  },
+  {
+    "revision": "92a095db30a46b25e29b",
+    "url": "/streams-client/static/js/2.fd40a86a.chunk.js"
+  },
+  {
+    "revision": "fdf88fcebc2fa1d81c359557abec9f5f",
+    "url": "/streams-client/index.html"
+  }
+];
