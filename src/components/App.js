b143b2ba3d53d54881384ada09e692ae58c3ad23
Yi-Shin lin <yishinlin@outlook.com>
2019-03-19 13:52:58 +0800

    Route+Link to

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route ,Link } from "react-router-dom";
 
 const PageOne = () => {
   return (
     <div>
-      PageOne <a href="/pagetwo">PageTwo</a>
+      PageOne {/*<a href="/pagetwo">PageTwo</a>*/}  
+      <Link to='/pagetwo'>PageTwo</Link>
     </div>
   );
 };
+//Bad to use anchor tag
 const PageTwo = () => {
   return (
     <div>
-      <a href="/">PageOne</a> PageTwo
+      <Link to="/">PageOne</Link> PageTwo
     </div>
   );
 };
@@ -22,6 +24,7 @@ const App = () => {
       <BrowserRouter>
         <div>
           <Route path="/" exact component={PageOne} />
+          {/*<Route path="/" exact component={PageOne} /> HTML宣告兩個就會產生兩個併排*/}
           <Route path="/pagetwo" exact component={PageTwo} />
         </div>
       </BrowserRouter>
